04e1ff06de3370681bbe602e079359f6abe25546
Mr. Sergei <gjxtibgznre@gmail.com>
2026-09-14 00:19:13 +0500

    Support building through source symlink

--- companion/build.sh
+++ companion/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
-cd "$(dirname "$0")"
+cd -P "$(dirname "$0")"
 mkdir -p 'build/Task Plan Companion.app/Contents/MacOS' 'build/Task Plan Companion.app/Contents/Resources' build/module-cache
 swiftc -swift-version 5 -module-cache-path build/module-cache main.swift -o 'build/Task Plan Companion.app/Contents/MacOS/TaskPlanCompanion' -framework AppKit -framework SwiftUI
 cp Info.plist 'build/Task Plan Companion.app/Contents/Info.plist'
